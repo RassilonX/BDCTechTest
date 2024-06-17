86687c06f21cf70cde8f43cded849bd79a7628a8
RassilonX <chrisfraser18@gmail.com>
2024-06-17 05:55:20 +0100

    Base implementation of the service layer

---
+++ BDCTechTest/BDCTechTest/Components/Services/IServiceBase.cs
@@ -0,0 +1,7 @@
+﻿namespace BDCTechTest.Components.Services;
+
+public interface IServiceBase
+{
+    public Task GetMOTDataAsync(string regNumber) =>
+        Task.FromResult(new { Message = "Service not implemented yet." });
+}
